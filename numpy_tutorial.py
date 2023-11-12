fa163319fa2d94d9640af2596742ac41bc667d98
sunghtun <ysh970718@gmail.com>
2023-11-12 11:16:08 +0000

    add plot code and perceptron logic AND,OR,NAND is good

--- numpy_tutorial.py
+++ numpy_tutorial.py
@@ -4,3 +4,16 @@ x = np.array([1.0, 2.0, 3.0])
 print(x)
 
 print(type(x))
+
+y = np.array([2.0, 4.0, 6.0])
+
+print(x + y)
+print(x * y)
+print(x / y)
+print(x - y)
+
+X = np.array([[51,55],[22,33],[21,45]])
+
+print(X.flatten())
+print(X > 30)
+print(X[X > 30])
