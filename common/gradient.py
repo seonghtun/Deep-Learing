cdf84d6794cb5c763eb4398d11fc892855b4158b
sunghtun <ysh970718@gmail.com>
2023-12-17 12:04:06 +0000

    add train_neralnet code

---
+++ common/gradient.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h) 계산
+    
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val # 값 복원
+
+    return grad
+
+def numerical_gradient(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val  # 값 복원
+        it.iternext()
+
+    return grad
+
+
+def function_2(x):
+    return x[0] ** 2 + x[1] **2 
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num = 100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f,x)
+        x -= lr * grad
+    return x
+
+if "__main__" == __name__:
+
+    x = np.arange(0.0, 20.0, 0.1)
+
+    print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+    print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+    print(numerical_gradient(function_2, np.array([3.0, 0.0])))
+
+    init_x = np.array([-3.0, 4.0])
+    print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
+    init_x = np.array([-3.0, 4.0])
+    print(gradient_descent(function_2, init_x=init_x, lr=10.0, step_num=100))
+    init_x = np.array([-3.0, 4.0])
+    print(gradient_descent(function_2, init_x=init_x, lr=1e-10, step_num=100))
