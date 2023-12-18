6ead5c20b43621c9045f179197fb115b58e18012
sunghtun <ysh970718@gmail.com>
2023-12-18 12:50:56 +0000

    feat gradient and add layers class

--- common/layers.py
+++ common/layers.py
@@ -29,3 +29,43 @@ class Sigmoid:
         dx = dout* (1.0 - self.out) * self.out
 
         return dx
+
+class Affine: # 행렬곱?
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+    
+    def forward(self,x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+    
+    def backward(self,dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T , dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self,x,t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
+
